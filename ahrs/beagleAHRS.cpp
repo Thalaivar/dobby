22ec804ccf94599f3c85e849336eb19bcf2133bb
Thalaivar <dhruvlaad@outlook.com>
2018-02-01 14:51:15 +0530

    new libraries added

---
+++ ahrs/beagleAHRS.cpp
@@ -0,0 +1,12 @@
+#include <beagleAHRS.h>
+
+
+void beagle_ahrs::run_complementary_filter_accel_gyro(float* accelData, float* gyroData, float* roll, float* pitch) {
+
+
+  }
+
+void beagle_ahrs::get_angles_accel(float* accelData, float* accel_roll, float* accel_pitch) {
+
+
+  }

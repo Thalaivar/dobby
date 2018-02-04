a26a4f71df516fddcf0d2c86535ae8788723c8ba
Demo User <debian@beaglebone.localdomain>
2018-02-04 05:47:23 +0000

    fixed imu issue

--- ahrs/test_ahrs.py
+++ ahrs/test_ahrs.py
@@ -10,9 +10,13 @@ from dobby_bmp import BMP280
 imu = MPU9250(MPU9250.ACCEL_4G, MPU9250.GYRO_250DPS, MPU9250.MAG_16BITS, MPU9250.MAG_100_HZ)
 bmp = BMP280()
 ahrs = AHRS()
-for i in range(2000):
-	if imu.is_data_ready():
-		imu.update()
-		ahrs.convert_accel_to_euler()
-	print ahrs.accel_pitch
+while True:
+	try:
+		if imu.is_data_ready():
+			imu.update()
+			ahrs.convert_accel_to_euler()
+			print ahrs.accel_pitch
+
+	except KeyboardInterrupt:
+		break
 
